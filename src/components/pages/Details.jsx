1cea951229793056640fb332bfe943d33bc3efc3
Guilherme <guilhermehenrique12308@gmail.com>
2023-11-22 20:42:54 -0300

    error page working and with dark mode

--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -4,15 +4,19 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 import Loading from "../layout/Loading";
+import Error from './Error'
 
 function Details() {
   const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(true);
+
   const { id } = useParams();
 
   async function ApiMovie(url) {
     const response = await fetch(url);
     const data = await response.json();
 
+    setLoading(false);
     setMovie(data);
   }
 
@@ -25,65 +29,83 @@ function Details() {
   return (
     <div className="lg:h-screen lg:flex lg:justify-center">
       <div className="mx-auto flex flex-col justify-center pt-28 p-10 max-w-3xl lg:max-w-7xl lg:flex-row lg:gap-6 lg:items-center lg:justify-center lg:h-screen">
-        {movie.id ? (
-          <>
-            <div className="flex flex-col items-center w-full lg:w-2/5">
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
-                alt={movie.title}
-                className="rounded w-full"
-              />
+        {loading && <Loading />}
 
-              <a
-                href={movie.homepage}
-                target="_blank"
-                className="btn my-3 w-full dark:bg-white dark:text-black"
-              >
-                Acessar pagina
-              </a>
-
-              <div className="flex gap-8 my-5">
-                <a href="https://github.com/guilhermeHenrique08">
-                  <FaGithub className="text-xl transition-all hover:scale-110 dark:text-white" />
+        {!loading &&(
+          movie.id ? (
+            <>
+              <div className="flex flex-col items-center w-full lg:w-2/5">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                  alt={movie.title}
+                  className="rounded w-full"
+                />
+  
+                <a
+                  href={movie.homepage}
+                  target="_blank"
+                  className="btn my-3 w-full dark:bg-white dark:text-black"
+                >
+                  Acessar pagina
                 </a>
-
-                <a href="https://www.linkedin.com/in/dev-guilherme-marques/">
-                  <FaLinkedin className="text-xl transition-all hover:scale-110 dark:text-white" />
-                </a>
-
-                <a href="#">
-                  <FaInstagram className="text-xl transition-all hover:scale-110 dark:text-white" />
-                </a>
+  
+                <div className="flex gap-8 my-5">
+                  <a href="https://github.com/guilhermeHenrique08">
+                    <FaGithub className="text-xl transition-all hover:scale-110 dark:text-white" />
+                  </a>
+  
+                  <a href="https://www.linkedin.com/in/dev-guilherme-marques/">
+                    <FaLinkedin className="text-xl transition-all hover:scale-110 dark:text-white" />
+                  </a>
+  
+                  <a href="#">
+                    <FaInstagram className="text-xl transition-all hover:scale-110 dark:text-white" />
+                  </a>
+                </div>
               </div>
-            </div>
-            <div className="w-full flex flex-col justify-center gap-5 border-white pb-5 lg:w-3/5 lg:border-b-2 dark:border-zinc-900">
-              <h2 className="font-bold text-lg lg:text-3xl dark:text-white">{movie.title}</h2>
-              <div className="my-5">
-                <h3 className="font-semibold lg:text-lg dark:text-white">Resumo</h3>
-                <p className="text-lg lg:text-base dark:text-white">{movie.overview}</p>
+              <div className="w-full flex flex-col justify-center gap-5 border-white pb-5 lg:w-3/5 lg:border-b-2 dark:border-zinc-900">
+                <h2 className="font-bold text-lg lg:text-3xl dark:text-white">
+                  {movie.title}
+                </h2>
+                <div className="my-5">
+                  <h3 className="font-semibold lg:text-lg dark:text-white">
+                    Resumo
+                  </h3>
+                  <p className="text-lg lg:text-base dark:text-white">
+                    {movie.overview}
+                  </p>
+                </div>
+                <div>
+                  <p className="text-gray-500 font-lg">
+                    Nota:{" "}
+                    <span className="text-black dark:text-white">
+                      {movie.vote_average}
+                    </span>
+                  </p>
+                  <p className="text-gray-500 font-lg">
+                    Data:{" "}
+                    <span className="text-black dark:text-white">
+                      {movie.release_date}
+                    </span>
+                  </p>
+                  <p className="text-gray-500 font-lg">
+                    Generos:{" "}
+                    <span className="text-black dark:text-white">
+                      {movie.genres.map((gender) => gender.name).join(", ")}
+                    </span>
+                  </p>
+                  <p className="text-gray-500 font-lg">
+                    Tempo:{" "}
+                    <span className="text-black dark:text-white">
+                      {movie.runtime} Minutos
+                    </span>{" "}
+                  </p>
+                </div>
               </div>
-              <div>
-                <p className="text-gray-500 font-lg">
-                  Nota: <span className="text-black dark:text-white">{movie.vote_average}</span>
-                </p>
-                <p className="text-gray-500 font-lg">
-                  Data: <span className="text-black dark:text-white">{movie.release_date}</span>
-                </p>
-                <p className="text-gray-500 font-lg">
-                  Generos:{" "}
-                  <span className="text-black dark:text-white">
-                    {movie.genres.map((gender) => gender.name).join(", ")}
-                  </span>
-                </p>
-                <p className="text-gray-500 font-lg">
-                  Tempo:{" "}
-                  <span className="text-black dark:text-white">{movie.runtime} Minutos</span>{" "}
-                </p>
-              </div>
-            </div>
-          </>
-        ) : (
-          <Loading />
+            </>
+          ):(
+            <Error/>
+          )
         )}
       </div>
     </div>
--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -5,12 +5,12 @@ import LinkButton from "../LinkButton";
 function Error() {
   return (
     <div className="mx-auto h-screen flex flex-col items-center justify-center">
-      <div className="bg-white flex flex-col gap-4 p-5 max-w-lg">
+      <div className="bg-white flex flex-col gap-4 p-5 max-w-lg dark:bg-zinc-950">
         <div className="flex items-center gap-5">
-          <FaRegSadTear className="text-xl"/>
-          <h1 className="text-xl font-bold">Pagina não encontrada</h1>
+          <FaRegSadTear className="text-xl dark:text-white"/>
+          <h1 className="text-xl font-bold dark:text-white">Pagina não encontrada</h1>
         </div>
-        <p className="text-gray-500">
+        <p className="text-gray-500 dark:text-white">
           Ah, não conseguimos encontrar a página que você está procurando. Tente
           voltar à página anterior ou apenas clique no botão abaixo para voltar
           a pagina inicial
